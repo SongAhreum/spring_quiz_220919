412d53db881453c5c9c80577497e885394aabe64
송아름 <thddkfmathd@naver.com>
2023-01-10 23:36:08 +0900

    mybatis select insert 퀴즈

--- src/main/java/com/quiz/SpringQuizApplication.java
+++ src/main/java/com/quiz/SpringQuizApplication.java
@@ -1,15 +1,27 @@
 package com.quiz;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import javax.sql.DataSource;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@SpringBootApplication 
+@ComponentScan(basePackages = "com.quiz")
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class SpringQuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringQuizApplication.class, args);
 	}
+	
 }
+
+
---
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -0,0 +1,36 @@
+package com.quiz.lesson03.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson03.dao.RealEstateDAO;
+import com.quiz.lesson03.model.RealEstate;
+
+@Service
+public class RealEstateBO {
+	
+	@Autowired
+	private RealEstateDAO realEstateDAO;
+	
+	public RealEstate getRealEstateById(int id) {
+		return realEstateDAO.selectRealEstateById(id);
+	}
+	
+	public List<RealEstate> getRealEstateListByRentPrice(int rentPrice) {
+		return realEstateDAO.selectRealEstateListByRentPrice(rentPrice);
+	}
+	
+	public List<RealEstate> getRealEstateListByAreaPrice(int area,int price) {
+		return realEstateDAO.selectRealEstateListByAreaPrice(area, price);
+	}
+	
+	public int addRealEstate(RealEstate realEstate) {
+		return realEstateDAO.insertRealEstate(realEstate);
+	}
+	//String address, int	area,String type,int price,Integer rentPrice
+	public int addRealEstateAsField(int realtorId,String address, int area,String type,int price,Integer rentPrice) {
+		return realEstateDAO.insertRealEstateAsField(realtorId,address,area,type,price,rentPrice);
+	}
+}
