cb2a6727f00e3b18d460c41053220ba284bd67f4
송아름 <thddkfmathd@naver.com>
2023-01-16 17:49:23 +0900

    my batis update delet 문제

--- src/main/java/com/quiz/lesson03/dao/RealEstateDAO.java
+++ src/main/java/com/quiz/lesson03/dao/RealEstateDAO.java
@@ -28,4 +28,10 @@ public interface RealEstateDAO {
 			@Param("price") int price,
 			@Param("rentPrice") Integer rentPrice
 			);
+	public int updateRealEstateById(
+			@Param("id") int id,
+			@Param("type") String type,
+			@Param("price") int price);
+	
+	public int deleteRealEstateById(int id);
 }
