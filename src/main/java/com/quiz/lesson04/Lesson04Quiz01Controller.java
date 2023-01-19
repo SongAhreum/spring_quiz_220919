90b07badd3ca51850d084d7c9ce5adb9c6ea4216
송아름 <thddkfmathd@naver.com>
2023-01-19 23:20:10 +0900

    Spring JSP : jsp와 form태그 (EL태그사용, MVC의 Model사용, 파라미터있는경우 없는경우 controller의 한메서드에서 처리하기 )

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -2,11 +2,14 @@ package com.quiz.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.model.Seller;
 
 @RequestMapping("/lesson04/quiz01")
 @Controller
@@ -16,7 +19,7 @@ public class Lesson04Quiz01Controller {
 	private SellerBO sellerBO;
 	
 	//http://localhost/lesson04/quiz01/add_seller_veiw
-	@RequestMapping("/add_seller_veiw")
+	@GetMapping("/add_seller_veiw")
 	public String addSellerView() {
 		// /WEB-INF/jsp/ (lesson04/addSeller) .jsp
 		return"lesson04/addSeller";
@@ -25,13 +28,26 @@ public class Lesson04Quiz01Controller {
 	@PostMapping("/add_seller")
 	public String addSeller(
 			@RequestParam("nickname") String nickname,
-			@RequestParam("profileImageUrl") String profileImageUrl,
-			@RequestParam("temperature") String temperature
+			@RequestParam(value="profileImageUrl",required=false) String profileImageUrl,
+			@RequestParam("temperature") double temperature
 			) {
 		sellerBO.addSeller(nickname, profileImageUrl, temperature);
 		return "lesson04/afterAddSeller";
 	}
 	
 	//SELECT
+	//http://localhost/lesson04/quiz01/seller_info
+	//http://localhost/lesson04/quiz01/seller_info?id=1
+	@GetMapping("/seller_info")
+	public String SellerInfo(@RequestParam(value="id",required=false) Integer id,Model model) {
+		Seller seller = null;
+		if(id==null) {
+			seller =sellerBO.getSellerInfo();
+		} else {
+			seller =sellerBO.getSeller(id);
+		}
+		model.addAttribute("seller", seller);
+		return "lesson04/sellerInfo";
+	}
 	
 }
--- src/main/java/com/quiz/lesson04/bo/SellerBO.java
+++ src/main/java/com/quiz/lesson04/bo/SellerBO.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.quiz.lesson04.dao.SellerDAO;
+import com.quiz.lesson04.model.Seller;
 
 @Service
 public class SellerBO {
@@ -11,7 +12,13 @@ public class SellerBO {
 	@Autowired
 	private SellerDAO sellerDAO;
 	
-	public void addSeller(String nickname,String profileImageUrl,String temperature) {
+	public void addSeller(String nickname,String profileImageUrl,double temperature) {
 		sellerDAO.insertSeller(nickname, profileImageUrl, temperature);
+	}
+	public Seller getSellerInfo() {
+		return sellerDAO.selectSellerInfo();
+	}
+	public Seller getSeller(int id) {
+		return sellerDAO.selectSeller(id);
 	}
 }
